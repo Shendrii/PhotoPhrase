c3660fc9f703e5b98658085327b6d85a7af15177
Shendri Kenneth Yamba <shendrikenneth.yamba@gmail.com>
2024-09-07 19:17:40 +0800

    Delete Image

--- photophrase/app/page.tsx
+++ photophrase/app/page.tsx
@@ -48,20 +48,6 @@ export default function Home() {
       </main>
       <footer className={styles.footer}>
         <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
           href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
