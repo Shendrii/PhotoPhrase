8218683a153c7094fa5385eec100f635193c048a
Shendri Kenneth Yamba <shendrikenneth.yamba@gmail.com>
2024-09-09 03:27:43 +0800

    ✨ Change title and center items in navbar
    Change title and center items in navbar

--- photophrase/app/page.tsx
+++ photophrase/app/page.tsx
@@ -1,12 +1,6 @@
-import styles from "./page.module.css";
 import { getPost } from "../_actions/postAction";
 
 export default async function Home() {
   const res = await getPost();
   console.log(res);
-
-
-export default function Home() {
-  return <div></div>;
-
 }
