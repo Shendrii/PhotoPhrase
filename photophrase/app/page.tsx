ae4820b89be4a36a5cdbf530f34baf9706603e2c
bachira <johnero1278@gmail.com>
2024-09-09 16:43:00 +0800

    add route �

--- photophrase/app/page.tsx
+++ photophrase/app/page.tsx
@@ -1,6 +1,42 @@
-import { getPost } from "../_actions/postAction";
+import Header from "./components/Header";
+import { Button } from "@mui/material";
 
-export default async function Home() {
-  const res = await getPost();
-  console.log(res);
+export default function Page() {
+  return (
+    <div>
+      <Header />
+
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          flexDirection: "column",
+        }}
+      >
+        <h1
+          style={{
+            fontSize: "2rem",
+          }}
+        >
+          Welcome to PhotoPhrase. Generate a caption for your images!✨
+        </h1>
+
+        <div>
+          <Button
+            variant="contained"
+            sx={{
+              backgroundColor: "primary.main",
+              color: "white",
+              padding: "10px 20px",
+              "&:hover": {
+                backgroundColor: "primary.dark",
+              },
+            }}
+          >
+            Generate your captions...
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
 }
---
+++ photophrase/app/pages/captions/page.tsx
@@ -0,0 +1,14 @@
+import Header from "@/app/components/Header";
+
+export default function Page() {
+  return (
+    <div>
+      <Header />
+      <h3
+        style={{ fontSize: "2rem", display: "flex", justifyContent: "center" }}
+      >
+        Lorem ipsum blah blah
+      </h3>
+    </div>
+  );
+}
